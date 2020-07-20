18880756a9d4a50c9640cba117ac5074d352da34
sunilsala88 <sunilsala88@gmail.com>
2020-07-20 18:16:08 +0530

    master

---
+++ java/hello.java
@@ -0,0 +1,12 @@
+/**
+ * hello
+ */
+public class hello {
+
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    int x=4;
+    int s=x*x;
+    System.out.println(s);
+  }
+}
