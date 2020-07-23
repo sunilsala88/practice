3cba82bd8ef7a45164c6b72e93d3888efcaacbb2
sunilsala88 <sunilsala88@gmail.com>
2020-07-23 18:04:35 +0530

    master

--- java/hello.java
+++ java/hello.java
@@ -1,11 +1,43 @@
-import java.util.*;
+
+import java.io.*; 
+import java.util.Scanner;
+
+import javax.xml.crypto.KeySelector.Purpose;
+
+
 
 public class hello {
+  
 
-  public static void main(String[] args) {
-    System.out.println("hello world");
-    int age=3997;
-    double w=77.4;
-    System.out.println(age);
+  public static void main(String[] args){
+    int a,b,sum;
+    try{
+      methodA();
+      Scanner in =new Scanner (new File("test.in"));
+      System.out.println("exit main");
+    }
+    catch(ArithmeticException ex){
+      System.out.println("fine caught");
+    }
+    catch(FileNotFoundException ex){
+      System.out.println("file except");
+    }
+    catch(Exception es){
+      System.out.println("new");
+    }
+    finally{
+      System.out.println("must run");
+    }
+    
+  }
+  public static void methodA(){
+try{
+  System.out.println(1/0);
+}
+finally{
+  System.out.println("finally");
+}
+System.out.println("exit methon a");
+
   }
 }
