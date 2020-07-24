6f942188a3715a65d3d4d943479524e85b2f1e11
sunilsala88 <sunilsala88@gmail.com>
2020-07-24 13:17:40 +0530

    master

--- java/hello.java
+++ java/hello.java
@@ -1,43 +1,22 @@
 
 import java.io.*; 
 import java.util.Scanner;
-
 import javax.xml.crypto.KeySelector.Purpose;
 
-
-
 public class hello {
-  
-
   public static void main(String[] args){
-    int a,b,sum;
-    try{
-      methodA();
-      Scanner in =new Scanner (new File("test.in"));
-      System.out.println("exit main");
+    Scanner scn =new Scanner(System.in);
+    int n=scn.nextInt();
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i;j++){
+        System.out.print("\t");
+      }
+      for(int k=i;k>=1;k--){
+        System.out.print("*\t");
+      }
+      System.out.println();
     }
-    catch(ArithmeticException ex){
-      System.out.println("fine caught");
-    }
-    catch(FileNotFoundException ex){
-      System.out.println("file except");
-    }
-    catch(Exception es){
-      System.out.println("new");
-    }
-    finally{
-      System.out.println("must run");
-    }
-    
-  }
-  public static void methodA(){
-try{
-  System.out.println(1/0);
-}
-finally{
-  System.out.println("finally");
-}
-System.out.println("exit methon a");
+
 
   }
 }
