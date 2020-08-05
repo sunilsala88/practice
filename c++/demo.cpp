c77c85698eab423bdce045dcbef17989e5dbae60
sunilsala88 <sunilsala88@gmail.com>
2020-08-05 23:20:22 +0530

    master

--- c++/demo.cpp
+++ c++/demo.cpp
@@ -1,13 +1,38 @@
 #include<bits/stdc++.h>
 
-using namespace std;
-int main(){
-  int n=20;
+using namespace std; 
+  
+int main() 
+{ 
+    // Initializing 2D vector "vect" with 
+    // values 
+    int n=5;
+    vector<vector<int> > vect{ { 0,1,1,1,1 }, 
+                               { 1,0,1,1,0 }, 
+                               { 1,1,0,1,1 },
+                               { 0,0,0,0,0 },
+                               { 1,1,0,1,0 } }; 
+  
+    // Displaying the 2D vector 
+    for (int i = 0; i < vect.size(); i++) { 
+        for (int j = 0; j < vect[i].size(); j++) 
+            cout << vect[i][j] << " "; 
+        cout << endl; 
+    } 
+    cout<<endl;
+    int count=0;
+    int i;
   for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-      cout<<i;
+    if(vect[i][0]==0 && vect[i][i]==0){
+      for(int j=0;j<n;j++){
+        if(vect[i][j]==0 && vect[j][i]==1 ){
+          count++;
+        }
+      }
+      if(count==n-1)cout<<i;
+      
     }
-    int a;
-    cout<<'\n';
   }
+ 
+    return 0; 
 }
