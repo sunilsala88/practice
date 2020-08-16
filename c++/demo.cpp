8f24a0d0d856c4412c06f1cf082a9b5f1655310f
sunilsala88 <sunilsala88@gmail.com>
2020-08-16 23:51:34 +0530

    master

--- c++/demo.cpp
+++ c++/demo.cpp
@@ -12,12 +12,7 @@ int main()
                                { 1,1,0,1,1 },
                                { 0,0,0,0,0 },
                                { 1,1,0,1,0 } }; 
-  
-    // Displaying the 2D vector 
-    for (int i = 0; i < vect.size(); i++) { 
-        for (int j = 0; j < vect[i].size(); j++) 
-            cout << vect[i][j] << " "; 
-        cout << endl; 
+
 
   }
  
